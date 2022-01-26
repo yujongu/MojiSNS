28d54936222b2f407828fcdb44c3f262dddfc945
jooha6082 <jooha6082@gmail.com>
2022-01-25 23:06:42 -0500

    Make homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import * as React from 'react';
+import {HashRouter, Route} from "react-router-dom";
+import BoxSx from './components/Boxes';
+import Home from "./pages/Home"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // <HashRouter>
+  //   <Route path="/" exact={true} component={Home} />
+  // </HashRouter>
+    <Home/>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,43 @@
+import React from "react"
+import "./Home.css"
+
+class Home extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+
+                <div class="left">
+
+                </div>
+
+                <div class="rightHalf">
+
+                </div>
+
+                <div class="leftHalf">
+
+                </div>
+
+                <div class="welcome">
+                    <h1>Welcome to Moji!</h1>
+                    <p><br></br><br></br>Moji is a social networking site where people can share thoughts and ideas.</p>
+                    <p><br></br>-------------------------------------------</p>
+                    <p><br></br>Created by<br></br><br></br>Jongu Yu<br></br>Juha Jeon<br></br>Jaseok Choi<br></br>Jooheon Lee<br></br></p>
+                </div>
+
+                <div class="logo">
+
+                </div>
+
+                <button type="login">login</button>
+                
+                <button type="signup">signup</button>
+
+            </React.Fragment>
+        );
+    }
+
+
+}
+
+export default Home;
