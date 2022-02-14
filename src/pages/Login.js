a2f792006105be6216d126024709f397aef53361
jooha6082 <jooha6082@gmail.com>
2022-02-13 22:23:31 -0500

    modify the login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,32 @@
 import React from "react"
-import "./Home.css"
+import "./Login.css"
 import { useNavigate } from "react-router-dom";
 
 function Login() {
     return(
-        <h1>hi</h1>
+
+        <React.Fragment>
+        
+            <div class="background">
+
+            </div>
+
+            <div class="box">
+
+            </div>
+
+            <div class="logo">
+
+            </div>
+
+            <div class="email">
+
+
+
+
+            </div>
+
+        </React.Fragment>
     );
 }
 
