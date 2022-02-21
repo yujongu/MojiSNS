831340c4149ce147c0d2a3007df2df3b4bd9ea37
Jooheon Lee <lee3036@purdue.edu>
2022-02-20 23:51:56 -0500

    add comment schema

--- Backend/db/models/comment.js
+++ Backend/db/models/comment.js
@@ -1,33 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({ // need to add more
-    USER_ID: { 
+const commentSchema = new Schema({
+    POST_ID: { 
         type: String,
         required: true
     },
 
-    TOPIC_ID: {
+    OWNER_ID: {
+        type: Number,
+        required: true
+    },
+
+    CONTENT: {
         type: String,
         required: true
     },
 
-    LIKES_COUNT: {
-        type: Number,
-        required: true
-    },
-
-    COMMENTS_COUNT: {
-        type: Number,
-        required: false
-    },
-
-    BODY: {
+    PARENT_ID: {
         type: String,
         required: true
     }
 
-}, { timestamps: true});
+}, { timestamps: true });
 
-const Post = mongoose.model('Post', postSchema); //first is collection name in db. second is object type
-module.exports = Post;
+const Comment = mongoose.model('Comment', commentSchema); //first is collection name in db. second is object type
+module.exports = Comment;
