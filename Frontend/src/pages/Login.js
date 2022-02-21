6f25a4942787188b7b4aacad0a7142cb00fa08fc
Jooheon Lee <lee3036@purdue.edu>
2022-02-20 20:42:18 -0500

    fix wrong commit

--- Frontend/src/pages/Login.js
+++ Frontend/src/pages/Login.js
@@ -1,26 +1,8 @@
 import React from "react"
-<<<<<<< HEAD
 import { useState, useEffect } from "react";
 import "./Login.css"
 import { useNavigate } from "react-router-dom";
 
-=======
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { FileDownload } from "@mui/icons-material";
-const theme = createTheme();
->>>>>>> jlee
 
 
 
