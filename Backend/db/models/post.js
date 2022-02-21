edfcbcabb7d97e1018eddad359561c5c3c2814b6
Jooheon Lee <lee3036@purdue.edu>
2022-02-20 20:03:08 -0500

    push to dev

--- Backend/db/models/post.js
+++ Backend/db/models/post.js
@@ -26,6 +26,7 @@ const postSchema = new Schema({ // need to add more
         type: String,
         required: true
     }
+
 }, { timestamps: true});
 
 const Post = mongoose.model('Post', postSchema);
