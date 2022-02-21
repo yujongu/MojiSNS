5a4e60be45ec51a25b13a9d8df53dd003ecefadb
Jooheon Lee <lee3036@purdue.edu>
2022-02-21 01:27:09 -0500

    add update to postAPI

--- Backend/db/routes/postAPI.js
+++ Backend/db/routes/postAPI.js
@@ -1,29 +1,56 @@
-const { application } = require("express");
+//const { application } = require("express");
 const express = require("express");
-const Post = require("./models/post");
+const Post = require("../models/post");
 const router = express.Router();
 
-router.get("/main", async (req, res) => { //maybe not url?
+//app.use url encoded
+
+//return all posts by newest
+router.get("/main", async (req, res) => {
     const posts = await Post.find().sort({ createdAt: -1});
     res.send(posts);
-  }); //this returns everything
+  });
   
-
+//create new post
 router.post("/main", async (req, res) => { //makes new post on POST request
     const post = new Post(req.body);
     await post.save(); //save this object to collection in db
     res.send(post);//maybe different?
 });
 
-// router.get("/single-blog", async (req, res))
-
 //get single blog
-router.get("/main/:id", async (req, res) => {
-    try {
-      const post = await Post.findOne({ _id: req.params.id });
-      res.send(post);
-    } catch {
-      res.status(404);
-      res.send({ error: "Post doesn't exist!" });
-    }
-  });
+router.get("/main/:id", (req, res) => { //async?
+    const id =  req.params.id;
+
+    Post.findById(id)
+      .then(result => {
+        res.render('react file page for post', { blog: result })
+        //res.send() //TODO:
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+});
+
+//delete
+router.delete("/main/:id", (req, res) => {
+  const id =  req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs'}) //redirect on frontend
+    })
+    .catch(err => {console.log(err)});
+})// delete all comments
+
+
+//update post
+router.patch("/main/:id",  (req, res) => {
+  const id = req.params.id;
+
+  //const post = await Post.findById(id)
+  
+  await Post.updateOne({ _id: id }, {BODY: req.body}) //not sure
+})
+module.exports = router;
