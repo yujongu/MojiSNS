221a65ae1b16095cc14c9eeb703c56a40f0cdc27
Jooheon Lee <lee3036@purdue.edu>
2022-02-20 23:47:20 -0500

    add post API

---
+++ Backend/db/postAPI.js
@@ -0,0 +1,32 @@
+const { application } = require("express");
+const express = require("express");
+const Post = require("./models/post");
+const router = express.Router();
+
+router.get("/main", async (req, res) => { //maybe not url?
+    const posts = await Post.find().sort({ createdAt: -1});
+    res.send(posts);
+  }); //this returns everything
+  
+
+router.post("/main", async (req, res) => { //makes new post on POST request
+    const post = new Post({
+        USER_ID: req.body.USER_ID,
+        TOPIC_ID: req.body.TOPIC_ID,
+        BODY: req.body.BODY
+    });
+    await post.save(); //save this object to collection in db
+    res.send(post);//maybe different?
+});
+
+// router.get("/single-blog", async (req, res))
+
+router.get("/main/:id", async (req, res) => {
+    try {
+      const post = await Post.findOne({ _id: req.params.id });
+      res.send(post);
+    } catch {
+      res.status(404);
+      res.send({ error: "Post doesn't exist!" });
+    }
+  });
