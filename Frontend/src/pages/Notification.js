ca82b3537bc0a2b0c7e243479243cea23c53380a
Jongu Yu <yujongu@gmail.com>
2022-03-29 02:56:21 -0400

    create a ui that notifies no notification to user

--- Frontend/src/pages/Notification.js
+++ Frontend/src/pages/Notification.js
@@ -1,17 +1,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import "./Notification.css";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { BackendConn } from "../constants/backendConn";
 import NotifItem from "./components/NotifItem/NotifItem";
 
 function Notification() {
-  var username = "steve";
-  var type = "Follow";
-  var content = "";
-  var time = "";
-  var read = false;
   const currUser = JSON.parse(localStorage.getItem("currentUser"));
 
   const [notificationData, setNotificationData] = useState([]);
@@ -20,43 +14,30 @@ function Notification() {
     retrieveNotifications();
   }, []);
 
-  React.useEffect(() => {
-    if (notificationData.length != 0) {
-      // var count = followerData.length;
-      // followerData.forEach((element) => {
-      //   if (
-      //     datePretty(element.FOLLOW_DATE).includes("days") ||
-      //     datePretty(element.FOLLOW_DATE).includes("years")
-      //   ) {
-      //     count--;
-      //   }
-      // });
-      // setTodayCount(count);
-      
-    }
-  }, [notificationData]);
+  React.useEffect(() => {}, [notificationData]);
 
   var retrieveNotifications = () => {
     axios
       .get(`${BackendConn}notification/getNotifications/${currUser._id}`)
       .then((res) => {
         if (res.status === 200) {
-          // if (res.data === "") {
-          //   alert("No user found under that username");
-          // } else {
-          //   //first remove myself from search res if exists
-          //   setFollowerData(res.data.FOLLOWER_USERS);
-          //   localStorage.setItem("currentUser", JSON.stringify(res.data));
-          // }
-          setNotificationData(res.data)
+          setNotificationData(res.data);
         }
       });
   };
   return (
     <main className="notifMainContainer">
       <div className="notifContainer">
-      {notificationData.map((notification, index) => (
-            <NotifItem
+        <h1>Notification Page</h1>
+        {notificationData.length === 0 ? (
+          <div className="zeroNotificationContainer">
+            <p>Sorry, you do not have any notifications...</p>
+          </div>
+        ) : (
+          <div></div>
+        )}
+        {notificationData.map((notification, index) => (
+          <NotifItem
             key={index}
             notifId={notification._id}
             userName={notification.SENDER_USER_ID.USER_USERNAME}
@@ -67,8 +48,7 @@ function Notification() {
             read={notification.VIEWED}
           />
           // console.log(notification)
-          ))}
-        
+        ))}
       </div>
     </main>
   );
